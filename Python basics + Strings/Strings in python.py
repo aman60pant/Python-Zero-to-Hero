65b2fd60da184f9b72bed0eed0a3dbdaea5b212b
aman60pant <aman60pant@gmail.com>
2025-10-01 23:29:41 +0530

    modified Strings in python.py in Python basics + Strings

--- Python basics + Strings/Strings in python.py
+++ Python basics + Strings/Strings in python.py
@@ -97,7 +97,6 @@ print("\nPrinting Raw String in HEX Format: ")
 print(String1)
 
 
-# Formatting of Strings 
 # Default order 
 String1 = "{} {} {}".format('Geeks', 'For', 'Life') 
 print("Print String in default order: ") 
