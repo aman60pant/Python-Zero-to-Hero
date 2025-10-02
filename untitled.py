81fd76b52a7396bd8b4bbc689bcd9616b36fa056
aman60pant <aman60pant@gmail.com>
2025-10-02 13:50:06 +0530

    modified untitled.py deleted function-1.py

--- untitled.py
+++ untitled.py
@@ -1,1 +1,9 @@
-print("\"Dear Aman,\n\tthis python course is nice. Thanks!\"")
+chai_type = "Ginger chai"
+customer_name = "Aman"
+
+print(f"Order for {customer_name} : {chai_type} please!")
+
+chai_description = "Aromatic and Bold"
+
+print(f"First world: {chai_description[:8]}")
+print(f"Last world: {chai_description[13:]}")
