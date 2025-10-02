331a9deab4f0ae52d2088cc7fecb6b8c63640604
aman60pant <aman60pant@gmail.com>
2025-10-02 22:59:43 +0530

    modified untitled.py in .

--- untitled.py
+++ untitled.py
@@ -5,6 +5,5 @@ print(f"Order for {customer_name} : {chai_type} please!")
 
 chai_description = "Aromatic and Bold"
 
-
 print(f"First world: {chai_description[:8]}")
 print(f"Last world: {chai_description[13:]}")
