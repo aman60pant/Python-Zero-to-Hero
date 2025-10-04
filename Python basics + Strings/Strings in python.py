acd06732dd422e3ab65aa35cc20ac81c78bb28ce
aman60pant <aman60pant@gmail.com>
2025-10-04 18:57:05 +0530

    modified Strings in python.py in Python basics + Strings

--- Python basics + Strings/Strings in python.py
+++ Python basics + Strings/Strings in python.py
@@ -89,7 +89,8 @@ print(String1)
 String1 = "This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
 print("\nPrinting in HEX with the use of Escape Sequences: ") 
 print(String1)
-  
+
+
 # Using raw String to 
 # ignore Escape Sequences 
 String1 = r"This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
