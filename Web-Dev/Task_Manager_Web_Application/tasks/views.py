5c0c826e13362fd8068a1751420ac961d0b9f9bc
aman60pant <aman60pant@gmail.com>
2025-10-08 09:42:03 +0530

    deleted 6177 files

--- Web-Dev/Task_Manager_Web_Application/tasks/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Task
-
-def taskList(request):
-    tasks = Task.objects.filter(user=request.user) if request.user.is_authenticated else []
-    return render(request, 'tasks/task_list.html', {'tasks': tasks})
