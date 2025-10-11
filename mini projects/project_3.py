5584232c3f0137a995e8a93c9b79bcb5b09c0b49
aman60pant <aman60pant@gmail.com>
2025-10-11 23:26:21 +0530

    modified project_3.py in mini projects

--- mini projects/project_3.py
+++ mini projects/project_3.py
@@ -1,1 +1,15 @@
-cup_size = input("Enter the size of cup you want ")
+cup_size = input("Enter the size of cup you want: ").lower()
+cup_quantity = int(input("Enter the no of cups you want: "))
+
+if cup_size == "small" or cup_size == "medium" or cup_size == "large":
+    if cup_size == "small":
+        bill = cup_quantity*10
+    elif cup_size == "medium":
+        bill = cup_quantity*15
+    elif cup_size == "large":
+        bill = cup_quantity*20
+    print(f"Perfect your bill for {cup_quantity} {cup_size} cups is: {bill}")
+else:
+    print("Enter a valid cup size!")
+
+# 10 15 20
