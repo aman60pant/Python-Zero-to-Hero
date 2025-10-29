f86922af44a4b1db66fdb15c172d3a6110cde72f
aman60pant <aman60pant@gmail.com>
2025-10-29 17:43:46 +0530

    modified Walrus_operator.py and untitled.py

--- untitled.py
+++ untitled.py
@@ -1,9 +1,255 @@
-chai_type = "Ginger chai"
-customer_name = "Aman"
+Course content
+32 sections • 251 lectures • 32h 8m total length
 
-print(f"Order for {customer_name} : {chai_type} please!")
 
-chai_description = "Aromatic and Bold"
-
-print(f"First world: {chai_description[:8]}")
-print(f"Last world: {chai_description[13:]}")
+Installation of Tools (VSCode and Python) 02:34
+VS Code Setup (Extensions and Themes) 01:50
+Get your code files here 00:02
+Meet your Instructor - Hitesh 05:24
+What is Programming..? 09:01
+Convert that into Python Code 08:05
+A Real World Python Code Intro 12:43
+Why to use Python 04:04
+Writing first Python code on MAC 10:41
+Writing first Python code on WINDOWS 07:53
+Get everything in Virtual Environment 12:15
+Organize Python Code like a Pro 06:51
+PEP8 and Zen of python 04:46
+Objects - Mutable and Immutable in Python 18:18
+Numbers, Booleans and Operators in Depth in Python 27:00
+String - Index, Slice and Encoding 12:23
+Tuples and Membership Testing 08:45
+Basics of List in Python 13:38
+Operator overloading and bytearray in python 10:24
+Set and frozenset in python 09:01
+Dictionary in Python 16:38
+Touch on Advance Data types like Collections 07:03
+Kettle Boiling Story Project 07:52
+Building a Snack System 07:10
+Building a Chai Price Calculator 05:41
+Building Smart Thermostat System 05:46
+Delivery Fees Waiver System 07:07
+Build a train seat information system 07:27
+Introduction to Loops 03:53
+Tea Token Dispenser 06:35
+Batch Chai Preparation 03:02
+Looping through list - Orders Name 02:38
+Why to use Enumerate 05:17
+Zip Can Combine Lists 03:57
+Introducing While Loop in Python 05:07
+Break, Continue and Loop Fallback 13:00
+Walrus Operator is Interesting in Python 13:23
+Dictionary in place of Match Case 08:53
+Functions - Reducing Duplication and Splitting Complex Tasks 14:30
+Functions - 3 More Features 12:32
+Scope and Named Space in Functions 12:04
+Non local vs Global scopes 09:07
+Handling Arguments in Function in Python 15:01
+Handle Multiple Return in Python 10:43
+Lambdas, Pure vs Impure functions 12:24
+Documenting your Functions and Built-in Functions 09:24
+Python Imports, Modules and Init File 14:41
+What are Comprehensions in Python? 06:39
+List Comprehensions in Python 08:33
+Set Comprehensions in Python 12:00
+Dictionary Comprehensions in Python 05:37
+Generator Comprehensions for Memory Optimization 07:07
+Generators with Yield and Next Methods 10:34
+Infinite Generators in Python 04:44
+Send Value to Generators 07:45
+Yield From and Close the Generators 08:55
+Decorators in Python 09:12
+Build a Logger with Decorator 05:56
+Build an Authorization Decorator 05:45
+Building your 1st Class and Object in Python 09:05
+Class and Object Namespace 08:19
+Attribute Shadowing in Python 06:14
+Self argument in python 07:31
+Constructors and Init in Python Classes 08:20
+Inheritance and Composition in Python Classes 18:02
+3 Ways to Access Base Class 07:19
+Method Resolution Order - MRO 08:02
+Static Methods in Python 05:43
+Classmethod vs Staticmethod 11:47
+Property decorator - Getter and Setter 08:05
+What is Error handling 05:29
+Try except else and finally 08:28
+Catching multiple exceptions 06:56
+Raise your own errors 03:18
+Creating custom exceptions 03:57
+Mini project with exception learning 07:09
+File handling with try except and with 08:46
+Code files for Thread and concurrency section 00:02
+What is Concurrency and Parallelism? 26:46
+What is Global Interpreter Lock - GIL 16:39
+Threads and lock in depth 27:12
+Multi Process with Queue and Value 19:20
+Code files for asyncio section 00:02
+Asyncio, Event loop, coroutines and await in python 32:05
+Mixing threads with asyncio in python 08:43
+Asyncio and MultiProcess in python 11:28
+Understanding Daemon vs Non-Daemon Threads 05:45
+Debugging and Profiling - Race condition and Deadlock in python 14:19
+Why pydantic is important 08:59
+The foundation of pydantic 07:36
+Pydantic Default conversions 06:09
+Mixing pydantic and typing in python 05:34
+Adding validations with Field 14:03
+Field and model validators in python 08:05
+Computed property in pydantic 07:16
+Advance Validation in pydantic 09:54
+Nested models in pydantic 07:55
+Self referencing models in pydantic 06:49
+Advance nested model patterns 10:16
+Best practice for pydantic model design 06:03
+Model dump and model dump json in serialization of pydantic 17:15
+Understanding Large Language Models (LLMs) 05:55
+Deep Dive into the GPT Architecture 09:07
+How LLMS Work under the Hood? 07:16
+Fundamentals of Tokenization in NLP 08:05
+Implementing a Custom Tokenizer in Python 04:15
+The Transformer Breakthrough: Google’s Paper on Attention 06:42
+Deep Diving into Vector Embeddings 09:09
+Role of Positional Encodings in Transformers 03:20
+Understanding Multi-Head Attention for Rich Context 05:19
+Configuring Your OpenAI Account 02:43
+Invoking OpenAI APIs with Python 04:53
+Creating and Setting Up Google Gemini Account 02:21
+Using Google Gemini with OpenAI-Compatible APIs 03:15
+Prompt Fundamentals: Encoding Instructions for LLMs 00:56
+Prompting Types: Zero-Shot, Few-Shot, One-Shot 03:53
+One-Shot Prompting for Deterministic Inference 03:23
+Few-Shot Prompting for Contextual Generalization 03:31
+Structured Outputs with Few-Shot Prompting 03:13
+Chain-of-Thought (CoT) for Reasoning 12:49
+Auto-CoT: Automated Reasoning Prompt Generation 08:47
+Persona-Based Prompting 05:22
+Introduction to Prompt Serialization Styles 02:00
+Alpaca Prompt Template for Instruction Tuning 02:49
+ChatML Schema: OpenAI’s Structured Prompt Format 01:30
+INST Format: LLaMA-2 Instruction Specification 01:54
+Ollama Overview: Local LLM Runtime Engine 02:24
+Dockerized Environment Setup for LLMs 04:03
+Running Ollama Models with Docker Runner 03:15
+Configuring OpenWebUI with Ollama Backend 07:24
+FastAPI Environment Setup & Dependencies 04:01
+Integrating Ollama with FastAPI & Python APIs 06:19
+Hugging Face Model Deployment – Section Intro 03:01
+Configuring and Securing Hugging Face Account 02:36
+Accessing Instruct-Tuned Models (Google Gemma) 01:58
+Installing and Using Hugging Face CLI Tools 02:37
+Model Downloading & Execution from HF Hub 02:48
+Agentic AI Fundamentals – Section Intro 01:01
+What Exactly Are AI Agents? (Core Concepts) 10:08
+Coding Your First AI Agent 22:37
+Enforcing Structured Outputs with Pydantic 07:34
+Building a CLI Coding Agent (Claude Code) from Scratch 09:50
+Intro to RAG & LangChain – Section Overview 00:45
+Defining the Core Problem in RAG Systems 04:12
+Naive Retrieval-Based Solution Approach 04:49
+RAG Pipeline – Indexing Workflow Explained 05:22
+RAG Pipeline – Retrieval Mechanism in Depth 05:36
+Local Vector DB Setup with Docker Compose 04:43
+LangChain Installation & Setup 03:08
+LangChain Document Loaders for PDF 03:38
+LangChain Document Chunking & Splitting 03:20
+LangChain Vector Store as Retrievers 06:08
+LangChain-Powered RAG Retrieval Execution 09:11
+Sync vs Async in RAG Architectures 02:42
+Introduction to Queues System Design for Async Setup 02:53
+Python RQ Setup Distributed Queues 02:15
+Setting up Redis and Valkey with Docker 02:09
+Worker Orchestration with Python RQ 04:00
+FastAPI Endpoints setup for chat Queue 02:25
+Asynchronous Message Enqueueing with FastAPI 04:28
+FastAPI Polling & Dequeuing Messages from Async Queues 02:23
+Running & Scaling Worker Nodes for Background Processing 05:50
+What is Multi Modal Agent? 02:50
+Sending Multimedia to LLM (Images) 05:21
+Section Intro - Why LangGraph is a Game-Changer for AI Agents 00:43
+Deep Dive into LangGraph – Core Concepts, Nodes and Edges 05:19
+Setting Up LangGraph – Installation and Environment Configuration 04:07
+Defining State in LangGraph for AI Agent Context 02:37
+Defining Nodes and Functions in LangGraph 03:46
+Connecting Nodes with Edges – Designing Complex AI Graph 02:22
+Testing and Debugging Your LangGraph AI Workflow 02:40
+Integrating AI LLMs into LangGraph 02:40
+Conditional Edges & Smart Routing 11:50
+What is Checkpointing? Enabling Persistence in AI Agent Workflows 02:09
+Setting Up MongoDB with Docker for LangGraph Checkpoint Storage 03:21
+Implementing MongoDB Checkpointer in LangGraph Workflow Graphs 10:53
+Section Intro - The Memory Layer in AI Agents 00:38
+What is Memory in AI and Agents 04:21
+Different Types of Memory Architectures in AI and Agent 02:49
+Short-Term Memory – Handling Context Windows 05:45
+Long-Term Memory – Persistent Knowledge 03:34
+Factual Memory for AI Agents 01:55
+Episodic Memory in AI Workflows 02:01
+Semantic Memory for General Knowledge 00:47
+Mem0 Setup with Python for AI Memory Layer 01:42
+Mem0 Configuration with Python for Agents 03:32
+Vector Database Setup with Docker for Memory 00:51
+Using Vector Databases for AI Agent Memory 10:14
+Section Intro to the Graph Memory 00:41
+What is a Graph in AI and Data Systems 02:43
+Why Graph Memory is Needed in AI Agents 03:00
+Introduction to Graph Databases Neo4j and Kuzu 00:58
+Setting Up Neo4j Cloud Instance for Graph Memory 02:52
+Basics of Cypher Query for Graph Databases 08:11
+Adding Graph Database Support for Memory Agent 01:29
+Testing Graph Memory Implementation in Agents 04:35
+Section Intro to Conversational Agentic AI 01:09
+Understanding Conversational AI for Agents 01:37
+The S2S and Chained Voice Agents 02:31
+Speech To Speech Voice Agents 02:04
+Understanding the Chained Pattern for Voice Agents 04:28
+Setting Up STT for Chained Conversational Agent 05:17
+Setting Up OpenAI GPT Completions for Chained Agent 03:32
+Setting Up TTS for Conversational AI Agents 07:19
+Building a Voice Based AI Cursor IDE Clone 07:14
+Section Intro to Model Context Protocol 00:42
+Understanding What Model Context Protocol (MCP) Is 07:34
+Exploring the Architecture of MCP 05:53
+Introduction to GIT series 05:55
+Git init and hidden folder 17:34
+Git commits and logs 16:35
+Git internal working and configs 27:46
+Git merge and git conflicts 39:36
+Git Diff and stashing 28:22
+Git rebase is not that scary 21:43
+Insight of pushing code to github 40:57
+How to make Pull Request and Open Source contribution 24:45
+Introduction to Docker and the Rise of Containerization in DevOps 01:23
+Real-World Problem That Docker Solves in Modern Development 06:20
+Understanding the Difference Between Docker and Virtual Machines 11:30
+How to Install Docker on Your System for Local Development 05:13
+Docker Containers vs Docker Images: What's the Difference? 10:33
+Introduction to Docker CLI and Commonly Used Commands 10:45
+Running Docker Containers Using the CLI with Practical Examples 09:44
+Working with Docker Images Through Command-Line Interface (CLI) 04:46
+Exploring Docker Container Commands for Management and Debugging 08:13
+Creating and Using a Dockerfile to Containerize Node.js Apps 19:23
+Best Practices to Optimize Docker Images for Speed and Performance 13:49
+Understanding and Implementing Port Mapping in Docker Containers 07:17
+Auto Port Mapping in Docker: Dynamic Exposure of Container Ports 10:12
+Publishing Docker Images to Docker Hub or Private Registries 10:00
+Building Optimized Multi-Stage Docker Images for Production Use 18:28
+Security Best Practices for Running Docker Containers Safely 04:30
+Understanding Docker Bridge Networking for Container Communication 14:42
+Creating and Using Custom Docker Bridges for Network Isolation 10:13
+Docker Other Modes of Networking 04:18
+Attaching Host Machine Volumes to Docker Containers for Data Sharing 10:43
+Creating and Managing Custom Named Volumes in Docker for Persistence 05:15
+Introduction to Docker Compose 12:21
+Networking in Docker Compose 04:13
+Volume in Docker Compose 02:21
+Custom Docker builds 06:54
+Introduction to Docker Orchestration and Why It’s Crucial for Production 11:03
+Creating and Configuring a New AWS Account for ECS Deployment 08:23
+Setting Up Amazon Elastic Container Registry (ECR) to Push Docker Images 06:19
+Launching and Configuring ECS Clusters to Run Docker Containers 07:13
+Defining ECS Tasks and Creating Task Definitions for Container Execution 08:30
+Deploying ECS Services with Load Balancer for High Availability 13:37
+Cleaning Up AWS ECS and ECR Resources to Avoid Unnecessary Billing 02:23
+Debugging and Fixing ECS Health Check Failures During Container Deployment 17:39
+farewell 00:39
