f909ba4b7484d6cd7345ce62de6934305262b5a5
aman60pant <aman60pant@gmail.com>
2025-11-09 13:12:00 +0530

    modified Unique First Letters.py in comprehension in python

--- comprehension in python/Unique First Letters.py
+++ comprehension in python/Unique First Letters.py
@@ -1,5 +1,5 @@
 words = ["apple", "banana", "cherry", "avocado", "blueberry", "kiwi"]
 
-unique = {x for row in words for x in row[0]}
+unique = {word[0].lower() for word in words}
 
-print(unique)
+print(unique)   # {'a', 'b', 'c', 'k'}
