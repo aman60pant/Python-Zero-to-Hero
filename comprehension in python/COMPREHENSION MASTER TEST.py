07c4e6b94f40dbdbc2763a5f9f743d188a85dfd4
aman60pant <aman60pant@gmail.com>
2025-11-09 23:05:34 +0530

    modified COMPREHENSION MASTER TEST.py in comprehension in python

--- comprehension in python/COMPREHENSION MASTER TEST.py
+++ comprehension in python/COMPREHENSION MASTER TEST.py
@@ -2,3 +2,9 @@
 # Create a list of squares of all numbers between 1 and 15, but only include those whose square is greater than 50 and less than 200.
 list_1 = [num*num for num in range(1, 16) if num*num > 50 and num*num <200]
 print(list_1)
+
+# Q2: Conditional Logic (List Comprehension)
+# For numbers from 1 to 10, create a list containing "Even" if number is even, and "Odd" otherwise.
+List_2 = ["Even" if x%2==0 else "Odd" for x in range(1, 11)]
+print(List_2)
+
