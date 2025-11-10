163c775a26f7bd3d1f10fa0ed000b06e60d7be73
aman60pant <aman60pant@gmail.com>
2025-11-10 20:17:10 +0530

    changed 5 files

---
+++ OOPS concepts/Building your 1st Class and Object in Python/03_CreateClass.py
@@ -0,0 +1,10 @@
+# Write a program to create a class Dog with a method bark() that prints "Woof!". Create two dog objects and make them bark.
+class Dog:
+    def bark(self):
+        print("Woof!")
+
+obj1 = Dog()
+obj1.bark()
+
+obj2 = Dog()
+obj2.bark()
