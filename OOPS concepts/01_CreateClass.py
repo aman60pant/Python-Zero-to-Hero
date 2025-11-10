07ef988526597a1b19ddb7a24560b9805e88bd71
aman60pant <aman60pant@gmail.com>
2025-11-10 20:05:46 +0530

    added 01_CreateClass.py to OOPS concepts

---
+++ OOPS concepts/01_CreateClass.py
@@ -0,0 +1,18 @@
+# Create a class Student with attributes name and age. Create two objects and print their details.
+class Student:
+    Name = None
+    Age = None
+
+
+
+stud_1 = Student()
+stud_1.Name = "Aman"
+stud_1.Age = 22
+print(stud_1.Name)
+print(stud_1.Age)
+
+stud_2 = Student()
+stud_2.Name = "Priyanshu"
+stud_2.Age = 20
+print(stud_2.Name)
+print(stud_2.Age)
